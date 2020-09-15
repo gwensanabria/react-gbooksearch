19950946170770977a636dca31d5d2155f3a6668
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-09-15 14:41:33 -0500

    basic hero/jumbotron

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Hero() {
+    return (
+        <div className="jumbotron text-center">
+            <div className="container text-light">
+                <h1 style={{color: 'black'}}>Google Book Search</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
