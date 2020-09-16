def0b28a0847da6f3bbb7e72a3178ab739ef653d
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-09-16 01:46:39 -0500

    fix heroku deployment issue

--- connect.js
+++ connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 //environment variables
 require('dotenv').config();
 const connection = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@cluster0.wtpuo.mongodb.net/Book?retryWrites=true&w=majority`;
+
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
--- server.js
+++ server.js
@@ -14,9 +14,16 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+// }
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+  }
 
 // API routes
 app.use(routes)
